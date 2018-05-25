b524b353568c15a3823810eb5f59600c2f2d9aef
theidi267 <timea.heidenreich@gmail.com>
2018-05-25 14:00:58 -0700

    binary search now passes tests

--- arrays/array_binary_search/array_binary_search.js
+++ arrays/array_binary_search/array_binary_search.js
@@ -1,25 +1,24 @@
+'use strict';
 
+function binarySearch(arr, key) {
 
-let binarySearch = function(arr, key) {
+  let start = 0;
+  let end = arr.length - 1;
 
-    let start = 0;
+  while (start <= end) {
 
-    let end = arr.length - 1;
-
-    while (start <= end) {
-        
-        const mid = start + Math.floor(arr.length/2);
-        
-        if (arr[mid] === key) {
-            return mid;
-        }
-        if (arr[mid] < key) {
-            start = mid + 1;
-        } else {
-            end = mid - 1;
-        }
+    const mid = start + Math.floor((end - start) / 2);
+    
+    if (arr[mid] === key) {
+      return mid;
     }
-    return -1;
+    if (arr[mid] < key) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return -1;
 }
 
-// module.exports = binarySearch;
+module.exports = binarySearch;
