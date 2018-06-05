64f597f5e65f34160b8ab27dbe02d2c8f34681ee
theidi267 <timea.heidenreich@gmail.com>
2018-06-05 10:45:28 -0700

    modifications

--- TREES/Tree/fizzBuzzTree.js
+++ TREES/Tree/fizzBuzzTree.js
@@ -2,13 +2,18 @@
 
 function fizzBuzzTree(tree) {
 
-  if (!root) return undefined;
-  if (root.value % 3 === 0 && root.value % 5) return 'FizzBuzz';
-  if (root.value % 3 === 0) return 'Fizz';
-  if (root.value % 5 === 0) return 'Buzz';
+  function intFizzBuzz(node){
 
-  fizzBuzzTree(root.left);
-  fizzBuzzTree(root.right);
+    if (!node) return undefined;
+    if (node.value % 3 === 0 && node.value % 5) node.value = 'FizzBuzz';
+    else if (node.value % 3 === 0) node.value = 'Fizz';
+    else if (node.value % 5 === 0) node.value = 'Buzz';
+
+    if (node.left) intFizzBuzz(node.left);
+    if (node.right) intFizzBuzz(node.right);
+  }
+  intFizzBuzz(tree.root);
+  return tree;
 }
 
 module.exports = fizzBuzzTree;
